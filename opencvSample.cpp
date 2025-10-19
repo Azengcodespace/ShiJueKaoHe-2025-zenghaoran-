4a77cd1149b5a32a7e3175d441b98feb1ff11942
Azengcodespace <2098028173@qq.com>
2025-10-19 22:05:15 +0800

    Add files via upload
    timu1 2 3分别是OpenCV题的前三题 都在一个项目里 已配置好OpenCV环境 视觉组面向对象考核题是C++的编程题 完成情况总结后续补充

---
+++ opencvSample.cpp
@@ -0,0 +1,49 @@
+﻿// opencvSample.cpp : 此文件包含 "main" 函数。程序执行将在此处开始并结束。
+//
+#include "opencv2/opencv.hpp"
+#include <opencv2/imgcodecs.hpp>
+#include <opencv2/highgui.hpp>
+#include <iostream>
+#include <string>
+using namespace cv;
+using namespace std;
+
+/*void main()
+{
+	string path = "Resources/test.png";
+	Mat img = imread(path);
+	imshow("Image", img);
+}*/
+// 运行程序: Ctrl + F5 或调试 >“开始执行(不调试)”菜单
+// 调试程序: F5 或调试 >“开始调试”菜单
+
+/*void main()
+{   
+	string path = "Resources/test_video.mp4";
+	VideoCapture cap(path); 
+	Mat img;
+	while (true)
+	{
+		cap.read(img);
+	
+		imshow("Image", img);
+		waitKey(20);
+	}
+}*/
+/*Mat img = imread("Resources/test.png"); //提前放置测试图片D://a.png
+		imshow("test", img);
+	waitKey(0);
+	return 0;*/
+void main()
+{
+	
+	VideoCapture cap(0);
+	Mat img;
+	while (true)
+	{
+		cap.read(img);
+
+		imshow("Image", img);
+		waitKey(20);
+	}
+}
